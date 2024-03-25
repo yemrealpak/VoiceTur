fd5f74de62aa67cbde9541c47d853862d898f83d
Yunus Emre Alpak <yemrealpak@outlook.com.tr>
2024-03-25 21:22:20 +0300

    first

---
+++ voc.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+def speech_tr():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+
+    try:
+        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+
+    return r.recognize_sphinx(audio)
+
